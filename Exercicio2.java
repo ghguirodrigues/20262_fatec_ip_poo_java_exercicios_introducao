c792be9e86564a069b59e37bc6e32f3e9617e9f8
ghGuiRodrigues <guirodriguesdosantos@gmail.com>
2026-08-20 16:27:51 -0300

    feat(ap01-java-ex): implementa calculadora combustivel

---
+++ Exercicio2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercicio2{
+    public static void main(String... args){
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite a distancia:");
+        double distancia = leitor.nextDouble();
+        System.out.println("Digite o consumo em km/l");
+        double consumo = leitor.nextDouble();
+        System.out.println("Preço do litro");
+        double preco = leitor.nextDouble();
+        double litros = distancia / consumo;
+        double custo = litros * preco;
+        System.out.println("Litors necessarios: " + litros);
+        System.out.printf("Custo total R$%.2f", custo);
+        leitor.close();
+    }
+}
